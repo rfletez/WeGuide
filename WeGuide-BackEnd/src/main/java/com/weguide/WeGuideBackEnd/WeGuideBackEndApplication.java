475756a046d9f6001ee0b35cc59d0f191150da6a
Rosny Fletez Jr. <rfletezjr@gmail.com>
2024-02-06 13:29:51 -0700

    Backend created

---
+++ WeGuide-BackEnd/src/main/java/com/weguide/WeGuideBackEnd/WeGuideBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.weguide.WeGuideBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeGuideBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeGuideBackEndApplication.class, args);
+	}
+
+}
